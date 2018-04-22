1050c85dfd12230a28c77299a2b54d0499a4fd72
Hunter Haydel <hhaydel@gmail.com>
2018-04-22 17:32:06 -0400

    Small Test to Try Updater

--- src/Windows/components/Main.tsx
+++ src/Windows/components/Main.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { default as styled } from "styled-components";
 import { User } from "../../Users";
 import UserComponent from "../../Users/components/User";
 
@@ -11,10 +10,8 @@ export default class MainWindow extends React.Component<Props, undefined> {
   render() {
     return (
       <div>
-        <h2>Users Seen</h2>
-        {this.props.users.map(user => (
-          <UserComponent key={user.name} user={user} />
-        ))}
+        <h2>Users Seen.</h2>
+        {this.props.users.map(user => <UserComponent key={user.name} user={user} />)}
       </div>
     );
   }
